c23af4d0647ff52c6352d1e4cf3e8573a4122b79
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-09-13 21:03:40 +0200

    Fix successful movement message

--- src/mazegame/Main.java
+++ src/mazegame/Main.java
@@ -35,7 +35,7 @@ class Main {
                 case "south": case "s":
                     if (maze.movePlayer(Mob.DIRECTION.SOUTH)) {
                         System.out.println();
-                        System.out.println("Moving NORTH...");
+                        System.out.println("Moving SOUTH...");
                     } else {
                         System.out.println();
                         System.out.println("Cannot move there!");
@@ -44,7 +44,7 @@ class Main {
                 case "east": case "e":
                     if (maze.movePlayer(Mob.DIRECTION.EAST)) {
                         System.out.println();
-                        System.out.println("Moving NORTH...");
+                        System.out.println("Moving EAST...");
                     } else {
                         System.out.println();
                         System.out.println("Cannot move there!");
@@ -53,16 +53,38 @@ class Main {
                 case "west": case "w":
                     if (maze.movePlayer(Mob.DIRECTION.WEST)) {
                         System.out.println();
-                        System.out.println("Moving NORTH...");
+                        System.out.println("Moving WEST...");
                     } else {
                         System.out.println();
                         System.out.println("Cannot move there!");
                     }
                     break;
-                case "quit": case "q": case "exit": case "ex":
+                case "quit": case "q": case "exit": case "x":
                     System.out.println();
                     System.out.println("Exiting...");
                     finish = true;
+                    break;
+                case "help": case "h":
+                    System.out.println();
+                    System.out.println("Goal:");
+                    System.out.println("\tYour goal is to take the player from");
+                    System.out.println("\tthe starting point to the exit of the maze.");
+                    System.out.println();
+                    System.out.println("Legend:");
+                    System.out.println("\t#   a wall");
+                    System.out.println("\t@   the player");
+                    System.out.println("\ts   starting position");
+                    System.out.println("\te   the exit of the maze");
+
+                    System.out.println();
+                    System.out.println("Commands:");
+                    System.out.println("\th, help:   this help");
+                    System.out.println("\tn, north:  go north");
+                    System.out.println("\ts, south:  go south");
+                    System.out.println("\te, east:   go east");
+                    System.out.println("\tw, west:   go west");
+                    System.out.println("\tq, quit:   quit the game");
+                    System.out.println("\tx, exit:   alias for \"quit\"");
                     break;
                 default:
                     System.out.println();
