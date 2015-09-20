c16f38ff6bb534e6e4fe87b2b53d99442a6355fb
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-09-20 02:59:58 +0200

    Add a generic class for things in the maze: the Entity class.

--- src/mazegame/core/End.java
+++ src/mazegame/core/End.java
@@ -2,26 +2,13 @@ package mazegame.core;
 
 import mazegame.server.Icon;
 
-public class End implements HasIcon, HasPlace {
-
-    private Place place;
+public class End extends Entity {
 
     public End(Place place) {
-        if (place == null) {
-            throw new NullPointerException("place");
-        }
-        if (! place.isWalkable()) {
-            throw new IllegalArgumentException(
-                    "place is not walkable");
-        }
-        this.place = place;
+        super(place);
     }
 
     public Icon getIcon() {
         return Icon.END;
-    }
-
-    public Place getPlace() {
-        return place;
     }
 }
---
+++ src/mazegame/core/Entity.java
@@ -0,0 +1,24 @@
+package mazegame.core;
+
+import mazegame.server.Icon;
+
+public abstract class Entity implements HasIcon, HasPlace {
+
+    private Place place;
+
+    public Entity(Place place) {
+        if (place == null) {
+            throw new NullPointerException("place");
+        }
+        if (! place.isWalkable()) {
+            throw new IllegalArgumentException(
+                    "place is not walkable");
+        }
+        this.place = place;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+}
+
