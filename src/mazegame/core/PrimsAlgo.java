07c3bf8bee373d403f6c062794bdc05b3c78f9aa
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-09-23 20:43:47 +0200

    Make PrimsAlgo return build a simple L shaped corridor for testing

--- src/mazegame/core/PrimsAlgo.java
+++ src/mazegame/core/PrimsAlgo.java
@@ -3,6 +3,9 @@
 package mazegame.core;
 
 import mazegame.util.graph.Spanning2DRectLattice;
+import mazegame.util.graph.Vertex;
+import mazegame.util.graph.Edge;
+import mazegame.util.Direction;
 
 public class PrimsAlgo extends GraphAlgo {
 
@@ -11,7 +14,17 @@ public class PrimsAlgo extends GraphAlgo {
         int graphRows = MapDimToGraphDim(mapRows);
         int graphCols = MapDimToGraphDim(mapCols);
         graph = new Spanning2DRectLattice(graphRows, graphCols);
-        // return a FAKE map
-        // TODO: trim edges in graph to generate a real prim's map
+        // return a FAKE map: just a big L
+        // first the downward corridor
+        for (int r=0; r<graph.getNumRows()-1; r++) {
+            Vertex current = graph.getVertex(r, 0);
+            graph.setEdge(current, Direction.SOUTH, Edge.LINK);
+        }
+        // now the bottom corridor
+        for (int c=0; c<graph.getNumCols()-1; c++) {
+            Vertex current =
+                graph.getVertex(graph.getNumRows()-1, 0);
+            graph.setEdge(current, Direction.EAST, Edge.LINK);
+        }
     }
 }
