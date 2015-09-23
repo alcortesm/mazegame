aa24e577567a9b2a99e2c20eb6283d3646a79964
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-09-23 22:41:14 +0200

    Unmock Spanning2DRectLattice (getVertex, getEdge, setEdge, toString)

--- src/mazegame/util/graph/Spanning2DRectLattice.java
+++ src/mazegame/util/graph/Spanning2DRectLattice.java
@@ -16,6 +16,8 @@ public class Spanning2DRectLattice {
 
     private int rows;
     private int cols;
+    private Edge[][] horizontals;
+    private Edge[][] verticals;
 
     public Spanning2DRectLattice(int rows, int cols) {
         if (rows < 1) {
@@ -26,17 +28,69 @@ public class Spanning2DRectLattice {
         }
         this.rows = rows;
         this.cols = cols;
+        horizontals = new Edge[rows][cols-1];
+        for (int r=0; r<rows; r++) {
+            for (int c=0; c<cols-1; c++) {
+                horizontals[r][c] = Edge.NO_LINK;
+            }
+        }
+        verticals = new Edge[rows-1][cols];
+        for (int r=0; r<rows-1; r++) {
+            for (int c=0; c<cols; c++) {
+                verticals[r][c] = Edge.NO_LINK;
+            }
+        }
     }
 
     public Vertex getVertex(int r, int c) {
         return new Vertex(r, c, this);
     }
 
-    public Edge getEdge(Vertex a, Direction d) {
-        return Edge.NO_LINK;
+    public Edge getEdge(Vertex v, Direction d) {
+        if (v == null) {
+            throw new NullPointerException("v");
+        }
+        int r = v.getRow();
+        int c = v.getCol();
+        switch (d) {
+            case NORTH:
+                return verticals[r-1][c];
+            case SOUTH:
+                return verticals[r][c];
+            case EAST:
+                return horizontals[r][c];
+            case WEST:
+                return horizontals[r][c-1];
+            default:
+                throw new UnsupportedOperationException(
+                        d.toString());
+        }
     }
 
-    public void setEdge(Vertex a, Direction d, Edge e) {
+    // TODO: repeating this switch from the getEdge method is ugly
+    public void setEdge(Vertex v, Direction d, Edge e) {
+        if (v == null) {
+            throw new NullPointerException("v");
+        }
+        int r = v.getRow();
+        int c = v.getCol();
+        switch (d) {
+            case NORTH:
+                verticals[r-1][c] = e;
+                break;
+            case SOUTH:
+                verticals[r][c] = e;
+                break;
+            case EAST:
+                horizontals[r][c] = e;
+                break;
+            case WEST:
+                horizontals[r][c-1] = e;
+                break;
+            default:
+                throw new UnsupportedOperationException(
+                        d.toString());
+        }
     }
 
     // the order of a graph is the number of its vertices
@@ -50,5 +104,59 @@ public class Spanning2DRectLattice {
 
     public int getNumCols() {
         return cols;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Spanning2DRectLattice: (" +
+                rows + ", " + cols + ")" +
+                System.lineSeparator() + System.lineSeparator());
+
+        // append vertex, east and south links of all vertex in all
+        // rows except the last one
+        for (int r=0; r<rows-1; r++) {
+            sb = appendVertexAndEastLinks(sb, r);
+            sb = appendSouthLinks(sb, r);
+        }
+        // append vertex and east links of vertex in the
+        // last row
+        sb = appendVertexAndEastLinks(sb, rows-1);
+
+        return sb.toString();
+    }
+
+    private StringBuilder appendVertexAndEastLinks(StringBuilder sb, int row) {
+        Vertex current;
+        sb.append("\t");
+        for (int c=0; c<cols; c++) {
+            sb.append("*");
+            if (c<cols-1) {
+                current = getVertex(row, c);
+                if (getEdge(current, Direction.EAST) ==
+                        Edge.LINK) {
+                    sb.append("--");
+                } else {
+                    sb.append("  ");
+                }
+            }
+        }
+        sb.append(System.lineSeparator());
+        return sb;
+    }
+
+    private StringBuilder appendSouthLinks(StringBuilder sb, int row) {
+        Vertex current;
+        sb.append("\t");
+        for (int c=0; c<cols; c++) {
+            current = getVertex(row, c);
+            if (getEdge(current, Direction.SOUTH) ==
+                    Edge.LINK) {
+                sb.append("|  ");
+            } else {
+                sb.append("   ");
+            }
+        }
+        sb.append(System.lineSeparator());
+        return sb;
     }
 }
