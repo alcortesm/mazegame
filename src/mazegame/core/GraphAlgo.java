708f254ac5e0fe33607cbbca9487c97f7acab326
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-09-23 22:43:24 +0200

    Write GraphAlgo.generateMap()

--- src/mazegame/core/GraphAlgo.java
+++ src/mazegame/core/GraphAlgo.java
@@ -1,8 +1,14 @@
 package mazegame.core;
 
 import mazegame.util.graph.Spanning2DRectLattice;
+import mazegame.util.graph.Vertex;
+import mazegame.util.graph.Edge;
+import mazegame.util.Direction;
+import mazegame.core.Space;
 
 abstract class GraphAlgo extends MapGeneratorAlgo {
+
+    private static final Space space = new Space();
 
     protected Spanning2DRectLattice graph;
 
@@ -14,8 +20,8 @@ abstract class GraphAlgo extends MapGeneratorAlgo {
         return (2*graphDim)-1;
     }
 
-    static protected int MapDimToGraphDim(int mapDim) {
-        return roundUpIntegerDivision(mapDim, 2);
+    static protected int GraphIndexToMapIndex(int graphIndex) {
+        return (2*graphIndex);
     }
 
     static private int roundUpIntegerDivision(int num, int div) {
@@ -28,13 +34,79 @@ abstract class GraphAlgo extends MapGeneratorAlgo {
         if (graph == null) {
             throw new NullPointerException("graph");
         }
+        Tile[][] tiles = createChessBoard();
+        tiles = turnLinksIntoSpaces(tiles);
+        return new Map(tiles);
+    }
+
+    private Tile[][] createChessBoard() {
         Tile[][] tiles = new Tile[mapRows][mapCols];
         Space s = new Space();
+        Wall w = new Wall();
         for (int r=0; r<mapRows; r++) {
             for (int c=0; c<mapCols; c++) {
-                tiles[r][c] = s;
+                if ((r + c) % 2 == 0) {
+                    tiles[r][c] = s;
+                } else {
+                    tiles[r][c] = w;
+                }
             }
         }
-        return new Map(tiles);
+        return tiles;
+    }
+
+    private Tile[][] turnLinksIntoSpaces(Tile[][] tiles) {
+        int graphRows = graph.getNumRows();
+        int graphCols = graph.getNumCols();
+        Vertex current;
+
+        // for all rows but the last...
+        for (int r=0; r<graphRows-1; r++) {
+            // for all columns but the last...
+            for (int c=0; c<graphCols-1; c++) {
+                // set south and east tiles
+                current = graph.getVertex(r, c);
+                setTileFromEdge(tiles, current, Direction.SOUTH);
+                setTileFromEdge(tiles, current, Direction.EAST);
+            }
+            // for the last column set only the south tile
+            current = graph.getVertex(r, graphCols-1);
+            setTileFromEdge(tiles, current, Direction.SOUTH);
+        }
+
+        // for the last row, set only the east tiles
+        for (int c=0; c<graphCols-1; c++) {
+            // set south and east tiles
+            current = graph.getVertex(graphRows-1, c);
+            setTileFromEdge(tiles, current, Direction.EAST);
+        }
+        return tiles;
+    }
+
+    private void setTileFromEdge(
+            Tile[][] tiles, Vertex vertex, Direction dir) {
+        int graphRow = vertex.getRow();
+        int graphCol = vertex.getCol();
+        int mapRow = GraphIndexToMapIndex(graphRow);
+        int mapCol = GraphIndexToMapIndex(graphCol);
+        if (graph.getEdge(vertex, dir) == Edge.LINK) {
+            switch (dir) {
+                case NORTH:
+                    tiles[mapRow-1][mapCol] = space;
+                    break;
+                case SOUTH:
+                    tiles[mapRow+1][mapCol] = space;
+                    break;
+                case EAST:
+                    tiles[mapRow][mapCol+1] = space;
+                    break;
+                case WEST:
+                    tiles[mapRow][mapCol-1] = space;
+                    break;
+                default:
+                    throw new UnsupportedOperationException(
+                            dir.toString());
+            }
+        }
     }
 }
