9bfd1efbc5d40c54b1f66dbd9fbae573fd4de2cb
Alberto Cortés Martín <alcortes@it.uc3m.es>
2015-09-23 20:09:17 +0200

    move generate map to GraphAlgo for future algorithms

--- src/mazegame/core/GraphAlgo.java
+++ src/mazegame/core/GraphAlgo.java
@@ -4,6 +4,8 @@ import mazegame.util.graph.Spanning2DRectLattice;
 
 abstract class GraphAlgo extends MapGeneratorAlgo {
 
+    protected Spanning2DRectLattice graph;
+
     GraphAlgo(int rows, int cols) {
         super(rows, cols);
     }
@@ -20,5 +22,19 @@ abstract class GraphAlgo extends MapGeneratorAlgo {
         // black magic to avoid using Math.ceil() on the result of
         // double division
         return (num + div - 1) / div;
+    }
+
+    public Map generateMap() {
+        if (graph == null) {
+            throw new NullPointerException("graph");
+        }
+        Tile[][] tiles = new Tile[mapRows][mapCols];
+        Space s = new Space();
+        for (int r=0; r<mapRows; r++) {
+            for (int c=0; c<mapCols; c++) {
+                tiles[r][c] = s;
+            }
+        }
+        return new Map(tiles);
     }
 }
--- src/mazegame/core/PrimsAlgo.java
+++ src/mazegame/core/PrimsAlgo.java
@@ -8,24 +8,10 @@ public class PrimsAlgo extends GraphAlgo {
 
     public PrimsAlgo(int rows, int cols) {
         super(rows, cols);
-    }
-
-    public Map generateMap() {
         int graphRows = MapDimToGraphDim(mapRows);
         int graphCols = MapDimToGraphDim(mapCols);
-
-        Spanning2DRectLattice graph =
-            new Spanning2DRectLattice(graphRows, graphCols);
+        graph = new Spanning2DRectLattice(graphRows, graphCols);
         // return a FAKE map
         // TODO: trim edges in graph to generate a real prim's map
-        Tile[][] tiles = new Tile[mapRows][mapCols];
-        Space s = new Space();
-        for (int r=0; r<mapRows; r++) {
-            for (int c=0; c<mapCols; c++) {
-                tiles[r][c] = s;
-            }
-        }
-        return new Map(tiles);
     }
-
 }
